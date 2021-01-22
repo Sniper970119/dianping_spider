09495e940105a63272bc0112d83861f989bc92f6
Sniper <359366783@qq.com>
2021-01-22 19:58:18 +0800

    更新日志结构

---
+++ utils/logger.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃CREATE BY SNIPER┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+
+"""
+
+import logging
+import logging.handlers
+
+LOG_FILENAME = 'dianping.log'
+logger = logging.getLogger()
+
+
+def set_logger():
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(process)d-%(threadName)s - '
+                                  '%(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    file_handler = logging.handlers.RotatingFileHandler(
+        LOG_FILENAME, maxBytes=10485760, backupCount=5, encoding="utf-8")
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+
+set_logger()
