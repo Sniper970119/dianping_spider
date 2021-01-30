b6c365e040dda5236edbface92524469a3f8f957
Sniper <359366783@qq.com>
2021-01-30 23:09:02 +0800

    请求提取

---
+++ utils/requests_utils.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃CREATE BY SNIPER┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+
+"""
+
+import requests
+
+from utils.config import global_config
+
+
+class RequestsUtils():
+    def __init__(self):
+        requests_times = global_config.getRaw('config', 'requests_times')
+
+        pass
+
+    def get_requests(self, url):
+        pass
+
+
+requests_util = RequestsUtils()
