2073962a2ab8b2d9fdfeb6fa246644f685dfb144
Sniper <359366783@qq.com>
2021-03-16 15:50:03 +0800

    更新main

---
+++ main.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+"""
+      ┏┛ ┻━━━━━┛ ┻┓
+      ┃　　　　　　 ┃
+      ┃　　　━　　　┃
+      ┃　┳┛　  ┗┳　┃
+      ┃　　　　　　 ┃
+      ┃　　　┻　　　┃
+      ┃　　　　　　 ┃
+      ┗━┓　　　┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃CREATE BY SNIPER┣┓
+        ┃　　　　         ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+          ┃ ┫ ┫   ┃ ┫ ┫
+          ┗━┻━┛   ┗━┻━┛
+
+"""
+
+from function.search import *
+from utils.config import global_config
+
+if __name__ == '__main__':
+    keyword = global_config.getRaw('detail', 'keyword')
+    need_first = True if global_config.getRaw('detail', 'need_first') is 'True' else False
+    need_pages = int(global_config.getRaw('detail', 'need_pages'))
+    s = Search()
+    s.search(keyword, need_first, need_pages)
