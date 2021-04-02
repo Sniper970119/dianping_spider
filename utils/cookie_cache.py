cb8e92e240811d204507eb14e975c61021b219e2
Sniper <359366783@qq.com>
2021-04-02 20:09:56 +0800

    更新 cookie池框架

--- utils/cookie_cache.py
+++ utils/cookie_cache.py
@@ -20,6 +20,8 @@
 
 """
 
+import schedule
+
 
 class CookieCache():
     def __init__(self):
@@ -31,15 +33,29 @@ class CookieCache():
         """
         初始化cookie，读取文件中cookie信息，添加review、detail标记
         """
-        pass
+        with open('cookies.txt', 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+        for line in lines:
+            # cookie detail_valid_tag   review_valid_tag
+            self.all_cookie.append([line, 0, 0])
 
     def check_cookie(self):
         """
         检查cookie，定时任务，恢复&去掉 review、detail标记
         """
         review_test_url = 'http://www.dianping.com/shop/F8oeMhRBwBa99Z70/review_all/p34'
-
         detail_test_url = 'http://www.dianping.com/shop/G1PUPaOlLNpU8Z1h'
+        for cookie in self.all_cookie:
+            pass
+
+    def timing_check(self):
+        """
+        定时任务，用于定时启动check_cookie
+        :return:
+        """
+        schedule.every().minute.at('00:00').do(self.check_cookie)
+        while True:
+            schedule.run_pending()
 
     def get_cookie(self, mission_type):
         """
@@ -48,6 +64,15 @@ class CookieCache():
         :return:
         """
         assert mission_type in ['review', 'detail']
+
+    def change_state(self, cookie, mission_type):
+        """
+        修改cookie状态
+        :param cookie:
+        :param mission_type:
+        :return:
+        """
+        pass
 
 
 cookie_cache = CookieCache()
