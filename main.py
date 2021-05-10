d1836876ca38a51bf86a4699b27b800c076e131e
Sniper <359366783@qq.com>
2021-05-10 17:42:59 +0800

    更新main.py

--- main.py
+++ main.py
@@ -23,8 +23,10 @@
 import argparse
 
 from function.search import Search
+from utils.spider_controller import controller
 from utils.config import global_config
 from utils.logger import logger
+from utils.spider_config import spider_config
 
 parser = argparse.ArgumentParser()
 
@@ -36,30 +38,24 @@ parser.add_argument('--review', type=int, required=False, default=0,
                     help='spider as custom(just review)')
 parser.add_argument('--shop_id', type=str, required=False, default='',
                     help='custom shop id')
+parser.add_argument('--need_more', type=bool, required=False, default=False,
+                    help='need detail')
 args = parser.parse_args()
 if __name__ == '__main__':
     # args.review = 1
     # args.normal = 0
     # args.shop_id = 'l8QDQukrl2tXhzmY'
     if args.normal == 1:
-        keyword = global_config.getRaw('detail', 'keyword')
-        need_first = True if global_config.getRaw('detail', 'need_first') is 'True' else False
-        need_pages = int(global_config.getRaw('detail', 'need_pages'))
-
-        s = Search()
-        s.search(keyword, need_first, need_pages)
+        controller.main()
     if args.detail == 1:
         from function.detail import Detail
 
         shop_id = args.shop_id
         logger.info('爬取店铺id：' + shop_id + '详情')
-        d = Detail()
-        d.get_detail(shop_id)
+        controller.get_detail(shop_id, detail=args.need_more)
     if args.review == 1:
         from function.review import Review
 
         shop_id = args.shop_id
-        logger.info('爬取店铺id：' + shop_id+ '评论')
-        r = Review()
-        r.get_review(shop_id)
-
+        logger.info('爬取店铺id：' + shop_id + '评论')
+        controller.get_review(shop_id, detail=args.need_more)
