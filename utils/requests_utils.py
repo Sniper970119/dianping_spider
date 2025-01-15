a0819cc9d3101fb7cbd405df9a68384bf91f081c
JingBh <jingbohao@yeah.net>
2025-01-15 11:46:17 +0800

    use cookie where needed

--- utils/requests_utils.py
+++ utils/requests_utils.py
@@ -217,7 +217,7 @@ class RequestsUtils():
         retry_time = self.get_retry_time()
         while True:
             retry_time -= 1
-            r = requests_util.get_requests(url, request_type='proxy, no cookie')
+            r = requests_util.get_requests(url, request_type='proxy, cookie')
             try:
                 # request handle v2
                 r_json = json.loads(r.text)
@@ -227,7 +227,7 @@ class RequestsUtils():
                     if cache.is_cold_start is True:
                         print('处理验证码,按任意键回车继续:', r_json['customData']['verifyPageUrl'])
                         input()
-                        r = requests_util.get_requests(url, request_type='proxy, no cookie')
+                        r = requests_util.get_requests(url, request_type='proxy, cookie')
                         cache.is_cold_start = False
                 # 前置验证码过滤
                 if r_json['code'] == 200:
